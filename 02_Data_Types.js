891031c24ee48c543ceb38d381ce05344f45abbd
AnkitaAndhare <ankitaandhare11@gmail.com>
2024-07-26 20:27:45 +0530

    Initial commit with all program files

---
+++ 02_Data_Types.js
@@ -0,0 +1,21 @@
+"use strict"  // treat all js code as newer version
+
+// aleart( 3+3 ); // we r suing nodejs, not browser
+
+let name = "Ankita"
+let age = 21
+let islogged = false
+
+// number 2 to power 53
+// bigint
+// string = ""
+// boolean = true or false
+// null = standalone value
+// undefined 
+// symbol = unique 
+
+//object 
+
+console.log(typeof name);
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);       // object 
